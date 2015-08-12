fd39dd8425543c9cd50567209acb0b5861c37665
Jon <jallgood@uw.edu>
2015-08-12 02:28:59 +0000

    updating for books/books and deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,5 +105,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/books/'
+STATIC_URL = '/bookalias/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = '/home/ubuntu/restapi/static'
